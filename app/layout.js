9ee0772937c2a9c451b36f525caebf05cafc2069
Evil Buck <buckleyrobinson@gmail.com>
2025-05-03 22:28:52 -0400

    Added Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter, Playfair_Display } from 'next/font/google';
 import { ThemeProvider } from '@/components/ThemeProvider';
+import Script from 'next/script';
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -21,6 +22,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} ${playfair.variable} font-sans`}>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-651NQTJ8T1"
+          strategy="afterInteractive"
+          async
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-651NQTJ8T1');
+          `}
+        </Script>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
         </ThemeProvider>
